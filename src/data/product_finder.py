6ca88378b6f6e1db3dbc8d9158754d5c4c994047
Henrique Naldy <Henrique.nordy@gmail.com>
2025-03-30 22:48:27 -0300

    feat: Implementing repositories interfaces

---
+++ src/data/product_finder.py
@@ -0,0 +1,11 @@
+from src.models.sqlite.repository.interfaces.products_repository import ProductsRepositoryInterface
+from src.models.redis.repository.interfaces.redis_repository import RedisRepositoryInterface
+
+class ProductiFinder:
+    def __init__(
+            self,
+            redis_repo: RedisRepositoryInterface,
+            products_repo: ProductsRepositoryInterface
+        ) -> None:
+        self.__redis_repo = redis_repo
+        self.__products_repo = products_repo
